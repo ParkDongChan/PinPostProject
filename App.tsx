efe979849775a7e60142157a31280892807042de
ParkDongChan <tajanboy0359@naver.com>
2024-10-07 03:16:03 +0900

    initial project

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@
  * @format
  */
 
+import { NaverMapView } from '@mj-studio/react-native-naver-map';
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -63,36 +64,9 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+      <NaverMapView style={{flex: 1}} />
+    </View>
   );
 }
 
@@ -114,5 +88,36 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
+
+//<SafeAreaView style={backgroundStyle}>
+//    <StatusBar
+//      barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//      backgroundColor={backgroundStyle.backgroundColor}
+//    />
+//    <ScrollView
+//      contentInsetAdjustmentBehavior="automatic"
+//      style={backgroundStyle}>
+//      <Header />
+//      <View
+//        style={{
+//          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//        }}>
+//        <Section title="Step One">
+//          Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+//          screen and then come back to see your edits.
+//        </Section>
+//        <Section title="See Your Changes">
+//          <ReloadInstructions />
+//        </Section>
+//        <Section title="Debug">
+//          <DebugInstructions />
+//        </Section>
+//        <Section title="Learn More">
+//          Read the docs to discover what to do next:
+//        </Section>
+//        <LearnMoreLinks />
+//      </View>
+//    </ScrollView>
+//  </SafeAreaView>
 
 export default App;
